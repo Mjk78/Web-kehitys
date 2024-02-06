62178efa5ff8703e271caac0d6d490bffd90b639
Mjk78 <mira23009@student.hamk.fi>
2024-02-06 16:46:20 +0200

    php

---
+++ kirjaudu.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ylläpito Kirjautuminen</title>
+</head>
+
+<body>
+    <h1>Kirjaudu ylläpitosivulle</h1>
+
+    <form action="yllapito.php" method="post">
+
+        <label for="kayttajatunnus">Käyttäjätunnus:</label>
+
+        <input type="text" name="kayttajatunnus" required><br>
+
+ 
+
+        <label for="salasana">Salasana:</label>
+
+        <input type="password" name="salasana" required><br>
+
+ 
+
+        <input type="submit" value="Kirjaudu">
+
+    </form>
+
+</body>
+
+</html>
